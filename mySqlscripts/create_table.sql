b4e0157602f929279c25d283a7baf9b11f3376e5
kevinbowe <kevinbowe1957@gmail.com>
2024-08-25 11:26:52 -0400

    Discard Obsolete / Unreferenced files.
    --------------------------------------------------
    deleted:    Docker_Build_Image_.zsh
    deleted:    Docker_Run_Image_.zsh
    deleted:    Dockerfile-MySql
    deleted:    EXPERIMENT.bash
    deleted:    Program_EXP_01_WORKS.cs
    deleted:    Program_ORIGINAL_FAILS_.cs
    deleted:    Program_ORIGINAL_Local+Debug_Works.cs
    deleted:    Program_UPDATE_WORKS.cs
    deleted:    mySqlscripts    create_table.sql

--- mySqlscripts/create_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS mydb.myothertable (
-	column_name VARCHAR(255), column_name_x VARCHAR(255)
-);
-INSERT INTO mydb.myothertable (column_name, column_name_x) VALUES ('other_value','X_value');
